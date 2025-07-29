b807c1da33ac257a160f55d874e4678896ada135
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2025-07-29 18:16:13 +0200

    Squash all commits after configuration restructurization

---
+++ configs/home/kiro/steering/language-policy.md
@@ -0,0 +1,12 @@
+# Language Policy
+
+All generated content written to files must be in English. This includes:
+
+- Source code (variable names, function names, class names)
+- Code comments
+- Documentation (README, docs, inline docs)
+- Specifications and design documents
+- Commit messages
+- Error messages and log strings
+
+In chat conversations, respond in the user's language. Only file output must be in English.
---
+++ configs/hosts/darwin/modules/default.nix
@@ -0,0 +1,134 @@
+{
+  config,
+  pkgs,
+  inputs,
+  outputs,
+  ...
+}: let
+  isRosetta =
+    if config.nixpkgs.hostPlatform == "aarch64-darwin"
+    then true
+    else false;
+in {
+  imports = [
+    ../../../options.nix
+    inputs.home-manager.darwinModules.home-manager
+    inputs.mac-app-util.darwinModules.default
+    inputs.nix-homebrew.darwinModules.nix-homebrew
+  ];
+
+  ### nix, nixpkgs, home-manager ##############################################
+
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+  # Determinate manages the Nix installation itself
+  # (https://github.com/DeterminateSystems/nix-installer)
+  nix.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+  # # Temporary workaround for direnv build issue:
+  # # https://github.com/NixOS/nixpkgs/issues/502464
+  # nixpkgs.overlays = [
+  #   (_: prev: {
+  #     direnv = prev.direnv.overrideAttrs (_: {
+  #       doCheck = false;
+  #     });
+  #   })
+  # ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "bak";
+    extraSpecialArgs = {
+      inherit inputs outputs;
+      userConfig = config.user;
+    };
+  };
+
+  ### users ###################################################################
+
+  system.primaryUser = config.user.name;
+
+  users.users.${config.user.name} = {
+    name = config.user.name;
+    home = config.user.homeDirectory;
+    description = config.user.fullName;
+  };
+
+  home-manager.users.${config.user.name} = {
+    home = {
+      username = config.user.name;
+      homeDirectory = config.user.homeDirectory;
+    };
+    imports = [
+      ../../../home
+      ../../../home/${config.user.name}
+    ];
+  };
+
+  ### shell ###################################################################
+
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+  environment.shells = [pkgs.zsh];
+
+  ### environment variables ####################################################
+
+  environment.variables.EDITOR = "vim";
+
+  ### system ##################################################################
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  ### homebrew ################################################################
+
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = isRosetta;
+    user = config.user.name;
+    taps = {
+      "homebrew/homebrew-core" = inputs.homebrew-core;
+      "homebrew/homebrew-cask" = inputs.homebrew-cask;
+    };
+    mutableTaps = false;
+  };
+
+  homebrew = {
+    enable = true;
+    taps = builtins.attrNames config.nix-homebrew.taps;
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+      upgrade = true;
+    };
+    brews = [
+      "btop"
+      "cocoapods"
+      "fd"
+      "iproute2mac"
+      "lrzsz"
+      "mas"
+      "podman"
+      "qemu"
+      "ripgrep"
+      "sevenzip"
+      "wget"
+      "zssh"
+    ];
+    casks = [
+      "android-studio"
+      "flutter"
+      "font-jetbrains-mono-nerd-font"
+      "ghostty"
+      "google-chrome"
+      "kiro"
+      "libreoffice"
+      "logi-options+"
+      "tunnelblick"
+      "utm"
+      "wacom-tablet"
+      "xquartz"
+    ];
+  };
+}
