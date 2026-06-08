904aab086f0283902ff8c320e0378af187ad0c11
Kamil Krawczyk <kamil.krawczyk87@gmail.com>
2026-06-08 20:31:27 +0200

    chore(darwin): Configure home-manager backup file extension
    - Set backupFileExtension to "bak" in home-manager configuration
    - Ensures backup files are properly named during module updates

--- configs/hosts/darwin/modules/default.nix
+++ configs/hosts/darwin/modules/default.nix
@@ -39,6 +39,7 @@ in {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    backupFileExtension = "bak";
     extraSpecialArgs = {
       inherit inputs outputs;
       userConfig = config.user;
